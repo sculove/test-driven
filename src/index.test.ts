235f4da5e1b864bec75c345e10af5692afe4cdaa
sculove <sculove@gmail.com>
2021-03-28 15:23:40 +0900

    :recycle::fire: 중복되는 의미의 테스트 삭제

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,22 +1,20 @@
 import Dallar from "./dallar";
 
 describe("다중 통화를 지원하는 Money 객체", () => {
-  test("$5 * 2 = 10$ 테스트", () => {
-    const five:Dallar = new Dallar(5);
-    expect(five.times(2).equals(new Dallar(10))).toBeTruthy();
-  });
-  test("Dallar의 side-effect. 여러번 times 하는 테스트", () => {
-    // Given
-    const five:Dallar = new Dallar(5);
-    
-    // When, Then
-    expect(five.times(2).equals(new Dallar(10))).toBeTruthy();
-    
-    // When, Then
-    expect(five.times(3).equals(new Dallar(15))).toBeTruthy();
-  });
-  test("Dallar 비교 테스트", () => {
-    // TODO: Null 이나 dallar가 아닌 object인 경우 추가 테스트가 필요함
-    expect(new Dallar(5).equals(new Dallar(5)));
+  describe("Dallar 객체 테스트", () => {
+    test("여러번 times 하는 테스트", () => {
+      // Given
+      const five:Dallar = new Dallar(5);
+      
+      // When, Then
+      expect(five.times(2).equals(new Dallar(10))).toBeTruthy();
+      
+      // When, Then
+      expect(five.times(3).equals(new Dallar(15))).toBeTruthy();
+    });
+    test("Dallar 비교 테스트", () => {
+      // TODO: Null 이나 dallar가 아닌 object인 경우 추가 테스트가 필요함
+      expect(new Dallar(5).equals(new Dallar(5)));
+    });
   });
 });
