8bffb9b69f0afb188e0de7105535c76fb1835f40
sculove <sculove@gmail.com>
2021-03-28 14:38:57 +0900

    :green_circle: dallar 곱하기 테스트 성공

---
+++ src/dallar.ts
@@ -0,0 +1,10 @@
+class Dallar {
+  public amount: number;
+  constructor(amount: number) {
+  }
+  times(mlutiplier: number) {
+    this.amount = 5 * 2;
+  }
+};
+
+export default Dallar;
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,5 @@
+import Dallar from "./dallar";
+
 describe("다중 통화를 지원하는 Money 객체", () => {
   test("$5 * 2 = 10$ 테스트", () => {
     const five:Dallar = new Dallar(5);
