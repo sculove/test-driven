23b420dd2b3dbb64a08ba91b6c323fad2f969c9b
sculove <sculove@gmail.com>
2021-03-29 14:09:06 +0900

    :recycle: bank의 rate를 map으로 구성

--- src/bank.ts
+++ src/bank.ts
@@ -1,13 +1,17 @@
 import Expression from "./expression";
 
+
+
 class Bank {
+  private table: Map<string, number> = new Map();
   reduce(value: Expression, currency: string) {
     return value.reduce(this, currency);
   }
   addRate(from:string, to:string, rate:number) {
+    this.table.set(`${from}_${to}`, rate);
   }
   rate(from:string, to:string) {
-    return from === "CHF" && to === "USD" ? 2 : 1;
+    return this.table.get(`${from}_${to}`);
   }
 }
 
