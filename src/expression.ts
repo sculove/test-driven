318ab50dcd9404fce8dcb9f3f06797cc08d1337a
sculove <sculove@gmail.com>
2021-03-29 14:49:37 +0900

    :alien: Expression의 times 인터페이스 추가

--- src/expression.ts
+++ src/expression.ts
@@ -2,7 +2,8 @@ import Bank from "./bank";
 import Money from "./money";
 
 interface Expression {
-  reduce(bank:Bank, currency: string): Money;
+  reduce(bank:Bank, currency: string): Expression;
+  times(multiplier: number): Expression;
 }
 
 export default Expression;
