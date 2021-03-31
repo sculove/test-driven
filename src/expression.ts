9621cbf816155e13553df5a22fe505f8720d8614
sculove <sculove@gmail.com>
2021-03-31 16:49:04 +0900

    :bug: expression.reduce의 결과물은 Money 여야 한다.

--- src/expression.ts
+++ src/expression.ts
@@ -2,7 +2,7 @@ import Bank from "./bank";
 import Money from "./money";
 
 interface Expression {
-  reduce(bank:Bank, currency: string): Expression;
+  reduce(bank:Bank, currency: string): Money;
   times(multiplier: number): Expression;
   plus(addend: Expression): Expression;
 }
