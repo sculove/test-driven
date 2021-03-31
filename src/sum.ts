ea6e7b788ffbca3f61c5a8b232a51ae95e25d959
sculove <sculove@gmail.com>
2021-03-31 16:50:21 +0900

    :red_circle: sum에서 amount 접근 오류

--- src/sum.ts
+++ src/sum.ts
@@ -6,7 +6,6 @@ class Sum implements Expression {
   constructor(public augend: Expression, public addend: Expression) {
   }
   reduce(bank:Bank, currency: string) {
-    // @ts-ignore
     const amount = this.augend.reduce(bank, currency).amount + this.addend.reduce(bank, currency).amount;
     return new Money(amount, currency);
   }
