914481a2b4cbbf138473078e670191c4f498cfc8
sculove <sculove@gmail.com>
2021-04-04 21:02:16 +0900

    :red_circle: setup 호출 테스트

--- src/part2/wasRun.test.ts
+++ src/part2/wasRun.test.ts
@@ -10,4 +10,12 @@ describe("xUnit 개발", () => {
     // Then
     expect(test.wasRun).toBeTruthy();
   });
+  test("setUp 테스트", () => {
+    // Given
+    const test = new WasRun("testMethod");
+    expect(test.wasSetup).toBeFalsy();
+    test.run();
+    // Then
+    expect(test.wasSetup).toBeTruthy();
+  });
 });
