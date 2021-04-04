34fb6bcc3d1fb9e10ce865d2bb5b47eccf5cf2f3
sculove <sculove@gmail.com>
2021-04-04 21:04:26 +0900

    :green_circle: setup 호출 테스트 성공

--- src/part2/wasRun.ts
+++ src/part2/wasRun.ts
@@ -1,10 +1,12 @@
 import TestCase from "./testCase";
 class WasRun extends TestCase{
   public wasRun:boolean;
+  public wasSetup:boolean;
   constructor(private target: string) {
     super(target);
   }
   testMethod() { 
+    this.wasSetup = true;
     this.wasRun = true;
   }
 }
