f98f667e901b5afb8359ae88c971305bb81f4175
sculove <sculove@gmail.com>
2021-04-04 19:59:17 +0900

    :red_circle: 테스트 호출여부 확인하기

---
+++ src/part2/wasRun.test.ts
@@ -0,0 +1,11 @@
+describe("xUnit 개발", () => {
+  test("테스트 호출 여부 확인하기", () => {
+    // Given
+    const test = new WasRun("testMethod");
+    expect(test.wasRun).toBeFalsy();
+    // When
+    test.run();
+    // Then
+    expect(test.wasRun).toBeTruthy();
+  });
+});
