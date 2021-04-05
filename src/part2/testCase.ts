2884213b2d25594d68fadbbe430b0731fafedf7b
sculove <sculove@gmail.com>
2021-04-05 22:07:54 +0900

    :recycle: wasRun을 testcase로 이동

--- src/part2/testCase.ts
+++ src/part2/testCase.ts
@@ -1,11 +1,14 @@
 class TestClass {
+  public wasRun:boolean;
   public wasSetup:boolean;
   constructor(protected name:string) {};
   setup() {
     this.wasSetup = true;
+    this.wasRun = false;
   }
   run() {
     this.setup(); 
+    this.wasRun = true;
     // @ts-ignore
     this[this.name]();
   }
--- src/part2/wasRun.ts
+++ src/part2/wasRun.ts
@@ -1,11 +1,10 @@
 import TestCase from "./testCase";
 class WasRun extends TestCase{
-  public wasRun:boolean;
   constructor(private target: string) {
     super(target);
   }
   testMethod() { 
-    this.wasRun = true;
+  
   }
 }
 
