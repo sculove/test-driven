a2a1ddc200ef2cb23683b7a69216fe721e7cbcd4
sculove <sculove@gmail.com>
2021-04-20 13:13:46 +0900

    :poop: play 변수 인라인 하기 - :poop:

--- src/refactoring/chapter01/printStatement.ts
+++ src/refactoring/chapter01/printStatement.ts
@@ -11,15 +11,14 @@ export function statement(invoice:TInvoice, plays: Record<string, TPlay>) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(play, perf); // Refactoring - VSCODE : module 범위의 function으로 추출
+    let thisAmount = amountFor(playFor(perf), perf); // Refactoring - VSCODE : module 범위의 function으로 추출
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
     totalAmount += thisAmount;
   }
   result += `총액: ${format(totalAmount/100)}\n`;
