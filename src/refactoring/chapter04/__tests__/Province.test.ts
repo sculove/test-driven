36f591883c7bb3a6dc65e3345c0b72101e134d14
sculove <sculove.pe.kr@navercorp.com>
2021-04-27 08:40:20 +0900

    :white_check_mark: 테스트 코드 추가

--- src/refactoring/chapter04/__tests__/Province.test.ts
+++ src/refactoring/chapter04/__tests__/Province.test.ts
@@ -5,5 +5,9 @@ describe("province", () => {
   test("shorfall", () => {
     const asia =  new Province(sampleProvinceData())
     expect(asia.shortfall).toEqual(5);
+  });
+  test("profit", () => {
+    const asia = new Province(sampleProvinceData())
+    expect(asia.profit).toEqual(230);
   })
 })
