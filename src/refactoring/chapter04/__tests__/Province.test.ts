131c6060bed270c4a275b0f89132871ed6601745
sculove <sculove.pe.kr@navercorp.com>
2021-04-27 08:52:52 +0900

    :white_check_mark: 꽃길 테스트

--- src/refactoring/chapter04/__tests__/Province.test.ts
+++ src/refactoring/chapter04/__tests__/Province.test.ts
@@ -2,11 +2,19 @@ import Province from "../Province";
 import { sampleProvinceData } from "../sampleData";
 
 describe("province", () => {
-  const asia =  new Province(sampleProvinceData())
+  let asia:Province;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData())
+  })
   test("shorfall", () => {
     expect(asia.shortfall).toEqual(5);
   });
   test("profit", () => {
     expect(asia.profit).toEqual(230);
-  })
+  });
+  test("change production", () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toEqual(-6)
+    expect(asia.profit).toEqual(292)
+  });
 })
