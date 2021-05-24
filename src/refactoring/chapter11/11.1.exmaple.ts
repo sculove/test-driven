c23145b74f8b855d59628b15c0f9a7fce4b2a4a5
sculove <sculove.pe.kr@navercorp.com>
2021-05-25 08:49:05 +0900

    :recycle: CQ 함수 분리

--- src/refactoring/chapter11/11.1.exmaple.ts
+++ src/refactoring/chapter11/11.1.exmaple.ts
@@ -1,14 +1,26 @@
-function alertForMiscreant(people) {
+function findMiscreant(people) {
   for(const p of people) {
     if(p === "조커") {
-      setOffAlrm();
       return "조커"
     }
     if(p === "사루만") {
-      setOffAlrm();
       return "사루만"
     }
   }
 }
 
-alertForMiscreant(people);
+function alertMiscreant(people) {
+  for(const p of people) {
+    if(p === "조커") {
+      setOffAlarm();
+      return;
+    }
+    if(p === "사루만") {
+      setOffAlarm();
+      return;
+    }
+  }
+}
+
+const found = findMiscreant(people);
+alertMiscreant(people);
